1b086b294536c6b4b724bd693b3cc57f9997d29a
Sudhindra Dondapati <sudhindra5678@gmail.com>
2020-09-10 16:35:06 +0530

    Minor Changes.

--- app/src/main/java/com/example/deltatask3/database/PokemonTypeConverter.java
+++ app/src/main/java/com/example/deltatask3/database/PokemonTypeConverter.java
@@ -8,13 +8,13 @@ import com.google.gson.Gson;
 public class PokemonTypeConverter {
 
     @TypeConverter
-    public static Pokemon stringToPokemon(String value){
-        if (value==null) return new Pokemon("Name");
-        return new Gson().fromJson(value,Pokemon.class);
+    public static Pokemon stringToPokemon(String value) {
+        if (value == null) return new Pokemon("Name");
+        return new Gson().fromJson(value, Pokemon.class);
     }
 
     @TypeConverter
-    public static String pokemonToString(Pokemon pokemon){
+    public static String pokemonToString(Pokemon pokemon) {
         return new Gson().toJson(pokemon);
     }
 }
